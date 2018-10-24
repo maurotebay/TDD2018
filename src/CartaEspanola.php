0544579483385e1b171e89b074a6bfb5e0d536b9
Mauro Tebay <maurotebay@gmail.com>
2018-10-24 14:22:14 -0300

    cada tipo de carta tiene su constructor y sus funciones

--- src/CartaEspanola.php
+++ src/CartaEspanola.php
@@ -3,5 +3,20 @@
 namespace TDD;
 
 class CartaEspanola implements CartaInterface{
+
+    protected $palo;
+    protected $numero;
+
+    public function __construct($palo, $numero){
+        $this->palo = $palo;
+        $this->numero = $numero;
+    }
+
+    public function obtenerNumero(){
+        return $this->numero;
+    }
     
+    public function obtenerPalo(){
+        return $this->palo;
+    }
 }
--- src/CartaPoker.php
+++ src/CartaPoker.php
@@ -4,4 +4,21 @@ namespace TDD;
 
 class CartaPoker implements CartaInterface{
 
+    protected $palo;
+    protected $numero;
+
+    public function __construct($palo, $numero){
+        $this->palo = $palo;
+        $this->numero = $numero;
+    }
+
+    
+    public function obtenerNumero(){
+        return $this->numero;
+    }
+    
+    public function obtenerPalo(){
+        return $this->palo;
+    }
+
 }
