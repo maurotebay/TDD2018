ba8244b0ca0eccdca754d2641f5e439f5ba5497a
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-11-24 19:49:42 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/CartaEspanola.php
+++ src/CartaEspanola.php
@@ -2,29 +2,29 @@
 
 namespace TDD;
 
-class CartaEspanola implements CartaInterface{
+class CartaEspanola implements CartaInterface {
 
     protected $palo;
     protected $numero;
-    private $palosValidos = ['Espada', 'Basto', 'Copa', 'Oro'];
-    private $numerosValidos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    private $palosValidos = [ 'Espada', 'Basto', 'Copa', 'Oro' ];
+    private $numerosValidos = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ];
 
-    public function __construct($palo, $numero){
-        if($this->cartaValida($palo, $numero)){
+    public function __construct($palo, $numero) {
+        if ($this->cartaValida($palo, $numero)) {
             $this->palo = $palo;
             $this->numero = $numero;
         }
     }
 
-    public function cartaValida($pal,$num){
-        return (in_array($pal,$this->palosValidos) && in_array($num, $this->numerosValidos));
+    public function cartaValida($pal, $num) {
+        return (in_array($pal, $this->palosValidos) && in_array($num, $this->numerosValidos));
     }
 
-    public function obtenerNumero(){
+    public function obtenerNumero() {
         return $this->numero;
     }
     
-    public function obtenerPalo(){
+    public function obtenerPalo() {
         return $this->palo;
     }
 }
